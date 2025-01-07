21de37bec253238d772282cbb0ce00b90fab9f0b
gitseoyeon <tjdus553926@gmail.com>
2025-01-07 17:40:40 +0900

    solve: 사이클 단어

--- 01월/1주차/[BOJ] 사이클 단어/Main.java
+++ 01월/1주차/[BOJ] 사이클 단어/Main.java
@@ -1,3 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> words = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            if (!isCycleWordExists(words, word)) {
+                words.add(word);
+            }
+        }
+
+        System.out.println(words.size());
+    }
+
+    public static boolean isCycleWordExists(List<String> words, String word) {
+        for (String w : words) {
+            if (isCycle(w, word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCycle(String word1, String word2) {
+        if (word1.length() != word2.length()) return false;
+
+        String combine = word1 + word1;
+        return combine.contains(word2);
+    }
 }
