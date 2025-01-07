5ea524eb34a91f1e9f05e53fbb4f103c5d227a56
gitseoyeon <tjdus553926@gmail.com>
2025-01-07 21:51:46 +0900

    solve: 아즈버의 이빨 자국

--- 01월/1주차/[PRGRMS] 베스트앨범/Solution.java
+++ 01월/1주차/[PRGRMS] 베스트앨범/Solution.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
+
+        // 장르별 재생 횟수
         HashMap<String, Integer> genreCnt = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
             genreCnt.put(genres[i], genreCnt.getOrDefault(genres[i], 0) + plays[i]);
@@ -10,12 +12,14 @@ class Solution {
         List<String> sortedGenres = new ArrayList<>(genreCnt.keySet());
         sortedGenres.sort((g1, g2) -> genreCnt.get(g2) - genreCnt.get(g1)); 
 
+        // 장르별 곡 정보
         HashMap<String, List<int[]>> saveInfo = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
             saveInfo.putIfAbsent(genres[i], new ArrayList<>());
             saveInfo.get(genres[i]).add(new int[]{plays[i], i});
         }
 
+        // 곡 정렬
         for (String genre : saveInfo.keySet()) {
             saveInfo.get(genre).sort((s1, s2) -> {
                 if (s2[0] != s1[0]) return s2[0] - s1[0];
@@ -23,6 +27,7 @@ class Solution {
             });
         }
 
+        // 2곡씩 선택
         List<Integer> result = new ArrayList<>();
         for (String genre : sortedGenres) {
             List<int[]> songs = saveInfo.get(genre);
