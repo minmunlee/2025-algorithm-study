06cb55246d34e03fceb01b9bf2da095082321e8b
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2025-01-09 00:14:42 +0900

    Min: 1월 1주차 문제풀이 (#4)
    * solve: 사이클 단어
    
    * solve: 베스트 앨범
    
    * solve: FrontEnd 개발자 찾기
    
    * solve: 언어별 개발자 분류하기
    
    * resolve: 아즈버의 이빨 자국

--- 01월/1주차/[BOJ] 사이클 단어/Main.java
+++ 01월/1주차/[BOJ] 사이클 단어/Main.java
@@ -1,3 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> words = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            boolean isCycle = false;
+            for (String w : words) {
+                if (w.length() == word.length() && (w + w).contains(word)) {
+                    isCycle = true;
+                    break;
+                }
+            }
+            if (!isCycle) {
+                words.add(word);
+            }
+        }
+
+        System.out.println(words.size());
+    }
 }
