41c73b965fcd76dc68ab6d0323785c8a8e8a6c20
gitseoyeon <tjdus553926@gmail.com>
2025-09-23 17:29:41 +0900

    rename: [캠프가는 영식] 클래스명 변경

--- 09월/3주차/[BOJ] 캠프가는 영식/Min.java
+++ 09월/3주차/[BOJ] 캠프가는 영식/Min.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Camp {
+public class Min {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(br.readLine());
