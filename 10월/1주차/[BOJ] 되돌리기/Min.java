01f1e0f077aebab6a91095e067b1b12829ee6897
gitseoyeon <tjdus553926@gmail.com>
2025-10-14 17:37:35 +0900

    rename: 클래스명 변경

--- 10월/1주차/[BOJ] 되돌리기/Min.java
+++ 10월/1주차/[BOJ] 되돌리기/Min.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Re {
+public class Min {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
