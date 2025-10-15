af971f38fbb2952f33d829b106982cde3b65c826
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2025-10-15 22:52:46 +0900

    Min: 10월 1주차 문제 풀이 (#118)
    * solve: DNA
    
    * solve: 되돌리기
    
    * solve: 학생 번호
    
    * solve: 카카오프렌즈 컬러링북
    
    * rename: 클래스명 변경

---
+++ 10월/1주차/[BOJ] 되돌리기/Min.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Min {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] texts = new String[N + 1];
+        int[] times = new int[N + 1];
+
+        texts[0] = ""; 
+        times[0] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+
+            if (op.equals("type")) {
+                String c = st.nextToken();
+                int time = Integer.parseInt(st.nextToken());
+
+                texts[i] = texts[i - 1] + c;
+                times[i] = time;
+
+            } else { 
+                int back = Integer.parseInt(st.nextToken());
+                int time = Integer.parseInt(st.nextToken());
+                int backTime = time - back;
+
+                int idx = i - 1;
+                while (idx > 0 && times[idx] >= backTime) {
+                    idx--;
+                }
+
+                texts[i] = texts[idx];
+                times[i] = time;
+            }
+        }
+
+        System.out.println(texts[N]);
+    }
+}
