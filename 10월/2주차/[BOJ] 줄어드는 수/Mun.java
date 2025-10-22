cac5fb0b1b42d1aea0d1eee93b941840823a2f01
시원 <105481797+muncool39@users.noreply.github.com>
2025-10-22 23:57:41 +0900

    Mun: 10월 2주차 문제 풀이 (#120)
    * solve: 사탕 박사 고창영
    
    * solve: 줄어드는 수
    
    * solve: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데

---
+++ 10월/2주차/[BOJ] 줄어드는 수/Mun.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Mun {
+    static ArrayList<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < 10; i++) {
+            dfs(i, i);
+        }
+        Collections.sort(list);
+        if (N > list.size()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(list.get(N - 1));
+        }
+    }
+
+    private static void dfs(long num, int last) {
+        list.add(num);
+        for (int next=0;next<last;next++) {
+            dfs(num * 10 + next, next);
+        }
+    }
+}
